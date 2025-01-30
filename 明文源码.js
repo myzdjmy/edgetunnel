aa6f31a9215ee0f729d1cbdedc2ef8308c53ccb1
myzdjmy <wshpcljr@gmail.com>
2025-01-30 20:14:04 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'myzdj';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLmZ4eGsuZGVkeW4uaW8=');
